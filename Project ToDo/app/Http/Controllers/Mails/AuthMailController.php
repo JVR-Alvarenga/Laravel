778a3efef3c67f3801412fa2ca1e7e4f6689fc1c
João Victor Ribeiro Alvarenga <156453131+JVR-Alvarenga@users.noreply.github.com>
2024-08-24 11:04:22 -0300

    Update AuthMailController

--- Project ToDo/app/Http/Controllers/Mails/AuthMailController.php
+++ Project ToDo/app/Http/Controllers/Mails/AuthMailController.php
@@ -5,15 +5,13 @@ namespace App\Http\Controllers\Mails;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use App\Jobs\SendRegisterMail;
 
 class AuthMailController extends Controller {
     public function sendEmail(Request $r) {
-        if($user = User::where('email', $r->email)->first()) {
-            SendRegisterMail::dispatch($user);
-            return redirect(route('login'));
-        }
-        return redirect(route('home'));
+        $user = User::where('email', $r->email)->first();
+        SendRegisterMail::dispatch($user);
+        return redirect(route('login'));
+        
     }
 }
